88130af708a1a144ebae924116440c7c12b99b5f
rafaelavlucas <rafaelalucas_91@hotmail.com>
2020-04-28 10:04:47 +0100

    Initial commit

---
+++ js/navigation.js
@@ -0,0 +1,50 @@
+// Main Nav 
+
+const
+    mainNav = document.querySelector('.mainNav'),
+    templateNav = `
+<div class="mainNav__logo">
+    <img src="assets/img.svg" alt="Logo">
+</div>
+<span class="mainNav__icon"></span>
+<div class="mainNav__wrapper">
+<div class="mainNav__links">
+    <a class="mainNav__linkItem" href="">Link</a>
+    <a class="mainNav__linkItem" href="">Link</a>
+    <a class="mainNav__linkItem" href="">Link</a>
+    <a class="mainNav__linkItem" href="">Link</a>
+</div>
+</div>`;
+
+mainNav.insertAdjacentHTML('beforeend', templateNav);
+
+
+// Sticky Nav Bar
+window.onscroll = function () {
+    if (window.pageYOffset >= 100) {
+        mainNav.classList.add("navSticky");
+    } else {
+        mainNav.classList.remove("navSticky");
+    };
+}
+
+
+// Open Menu Mobile
+const iconNav = document.querySelector('.mainNav__icon');
+iconNav.addEventListener('click', openNavMobile);
+
+function openNavMobile() {
+    if (mainNav.classList.contains('navOpen')) {
+        mainNav.classList.remove('navOpen');
+    } else {
+        mainNav.classList.add('navOpen');
+    }
+}
+
+
+
+// Footer
+const footer = document.querySelector('.footer'),
+    templateFooter = `<p>teste</p>`;
+
+footer.insertAdjacentHTML('beforeend', templateFooter);
