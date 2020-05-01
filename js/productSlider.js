e588f9bf6e4d9cc968ec63789bee69c51709c393
rafaelavlucas <rafaelalucas_91@hotmail.com>
2020-05-01 23:56:17 +0100

    upd: product grid

--- js/productSlider.js
+++ js/productSlider.js
@@ -1,16 +1,17 @@
 products.forEach(function (el) {
     let templateSlider = `
 <div class="swiper-slide anime">
-<a class="productSlider__card" href="${el.url}">
-<figure class="productSlider__image"><img src="${el.img}" alt="${el.alt}"></figure>
-<div class="productSlider__text">
-<h5 class="productSlider__productTitle">${el.title}</h5>
-<p class="productSlider__productSubtitle">${el.subtitle}</p>
-<p class="productSlider__link">see details</p>
-</div>
-</a>
-</div>
-`;
+    <a class="productSlider__card" href="${el.url}">
+        <figure class="productSlider__image">
+            <img src="${el.img}" alt="${el.alt}">
+        </figure>
+        <div class="productSlider__text">
+            <h4 class="productSlider__productTitle">${el.title}</h4>
+            <h5 class="productSlider__productSubtitle">${el.subtitle}</h5>
+            <p class="productSlider__link">see details</p>
+        </div>
+    </a>
+</div>`;
     document.querySelector(".productSlider .swiper-wrapper").insertAdjacentHTML("beforeend", templateSlider);
 });
 
--- js/products.js
+++ js/products.js
@@ -1,37 +1,99 @@
 const products = [{
-        img: "assets/images/img01.jpg",
+        img: "assets/images/img12.jpg",
         title: "product 1",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: true,
+        young: false,
+        bio: false,
+
     },
     {
-        img: "assets/images/img02.jpg",
+        img: "assets/images/img13.jpg",
         title: "product 2",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: true,
+        young: false,
+        bio: false,
     },
     {
-        img: "assets/images/img03.jpg",
+        img: "assets/images/img14.jpg",
         title: "product 3",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: false,
+        young: false,
+        bio: true,
     },
     {
-        img: "assets/images/img04.jpg",
+        img: "assets/images/img15.jpg",
         title: "product 4",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: false,
+        young: true,
+        bio: false,
     },
     {
-        img: "assets/images/img05.jpg",
+        img: "assets/images/img16.jpg",
         title: "product 5",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: false,
+        young: true,
+        bio: false,
     },
     {
-        img: "assets/images/img01.jpg",
+        img: "assets/images/img17.jpg",
         title: "product 6",
         subtitle: "subtitle",
-        url: "#"
+        url: "#",
+        premium: false,
+        young: true,
+        bio: false,
     },
 ];
+
+products.forEach(function (el) {
+    let templateProducts = `
+    <div class="productGrid__product anime">
+        <div class="productGrid__card">
+            <figure class="productGrid__image">
+                <img src="${el.img}" alt="${el.title}">
+            </figure>
+            <div class="productGrid__text">
+                <h4 class="productGrid__productTitle">${el.title}</h4>
+                <h5 class="productGrid__productSubtitle">${el.subtitle}</h5>
+                <div class="ctaContainer">
+                    <a href="${el.url}" class="cta cta01">view details</a>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+    let templateBio = `
+    <figure class="contentMedia__image anime">
+        <img src="${el.img}" alt="${el.title}">
+    </figure>
+    <div class="contentMedia__content">
+        <h2 class="contentMedia__title anime">${el.title}</h2>
+        <h3 class="contentMedia__subtitle anime">${el.subtitle}</h3>
+        <p class="contentMedia__description anime">Far far away, behind teste</p>
+        <div class="ctaContainer anime">
+            <a href="${el.url}" class="cta cta01">view details</a>
+        </div>
+    </div>`;
+
+    if (el.premium) {
+        document.querySelector(".premium .productGrid__content").insertAdjacentHTML("beforeend", templateProducts);
+    };
+
+    if (el.young) {
+        document.querySelector(".young .productGrid__content").insertAdjacentHTML("beforeend", templateProducts);
+    };
+    if (el.bio) {
+        document.querySelector(".bio .contentMedia__wrapper").insertAdjacentHTML("beforeend", templateBio);
+    };
+
+});
